e4d20af9c9c343fed9f317dae2653ea61c1a124d
Lubellum <90450493+Lubellum@users.noreply.github.com>
2022-01-29 19:33:54 +0900

    メインループの作成

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // [1]ヘッダーをincludeする場所
-
+#include <iostream>
+using namespace std;
 // [2]定数を宣言する場所
 
 // [3]列挙定数を宣言する場所
@@ -11,5 +12,10 @@
 // [6]関数を宣言する場所
 // [6-?]プログラムの実行開始を宣言する
 int main (){
-  
+    // [6-?-?]メインループを追加する
+    while (1){
+      cout << "Hello World!";
+      return 0;
+    }
+    
 }
