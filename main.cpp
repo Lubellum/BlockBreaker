bc3c5ef99c32c77a2565a131a02ceae951b82e07
Lubellum <azukyun0202@gmail.com>
2022-02-24 19:40:51 +0900

    ボールの進行方向を宣言

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@ int field  [FIELD_HEIGHT][FIELD_WIDTH]; // [5-?]フィールドを宣言する
 int screen [FIELD_HEIGHT][FIELD_WIDTH]; // [5-?]画面を宣言する
 
 VEC2 ballPosition = { 0, FIELD_HEIGHT / 2 }; //[5-?]ボールの座標を初期化する
+VEC2 ballDirection = { 1, -1 };              // [5-?]ボールの進行方向を宣言する
 
 // [6]関数を宣言する場所
 // [6-?]画面を描画する関数を宣言
