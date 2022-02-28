9f0152c68c3f2ebc3d180830a29fa68cf60fc15e
Lubellum <azukyun0202@gmail.com>
2022-02-28 17:32:32 +0900

    パドルの座標を宣言

--- main.cpp
+++ main.cpp
@@ -43,6 +43,9 @@ int screen [FIELD_HEIGHT][FIELD_WIDTH]; // [5-?]画面を宣言する
 VEC2 ballPosition  = { 0, FIELD_HEIGHT / 4 }; //[5-?]ボールの座標を初期化する
 VEC2 ballDirection = { 1, 1 };                // [5-?]ボールの進行方向を宣言する
 
+VEC2 paddlePosition = {( FIELD_WIDTH - PADDLE_WIDTH) / 2, FIELD_HEIGHT - 2}; // [5-?]パドルの座標を宣言する
+
+
 // [6]関数を宣言する場所
 // [6-?]画面を描画する関数を宣言
 void DrawScreen(){
