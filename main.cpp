2a9bf23a19d6970cbcdd6bd9cf79948e9e4dfb61
Lubellum <azukyun0202@gmail.com>
2022-03-07 17:58:33 +0900

    入力されたキーによってパドルの処理を分岐

--- main.cpp
+++ main.cpp
@@ -148,6 +148,15 @@ int main (){
         // [6-?-?]キーボード入力があったかどうか判定する
         if(_kbhit()){
             VEC2 nextPaddleposition = paddlePosition; // [6-?-?]次のパドルの座標を宣言する
+            // [6-?-?]入力されたキーによって分岐する
+                switch (_getch()){
+                case 0x4b:
+                    paddlePosition.x--;
+                    break;
+                case 0x4d:
+                    paddlePosition.x++;
+                    break;
+            }
         }
     }
 }
